1187c8fcebd9501d45650eb6ad4eda4ba744d268
Lin Ma <malin84@gmail.com>
2024-07-02 15:35:29 +0800

    Update define_path.m

--- sample_codes/codes/define_path.m
+++ sample_codes/codes/define_path.m
@@ -1,30 +1,7 @@
 % The common block that defines paths and file names
 
-root_path       = '/home/lin/drop/Research/China_Econ_Geography/';
-% All the maps
-map_path        = [root_path 'Data/maps/3_codes/output/'];
-map_output      = map_path;
-aligned_map_path = [root_path 'Data/maps/3_codes/align_maps/output/'];
-
 % The baseline map at the 8k resolution
 base_map_path   = ['input/base_8k.jpg'];
 
 % Pixel Data
 pixel_data_path = ['../pixel_info/'];
-
-% The coordinates of cities and counties
-coordinate_path = [root_path 'Data/city_county_lists/'];
-
-
-root_path_win   = '../../../China_Econ_Geography/';
-
-% All the maps
-map_path_win        = [root_path_win 'Data/maps/3_codes/output/'];
-map_output_win      = map_path_win;
-aligned_map_path_win = [root_path_win 'Data/maps/3_codes/align_maps/output/'];
-
-% The baseline map at the 8k resolution
-base_map_path_win   = [root_path_win 'Data/maps/2_processed_maps/base_8k.png'];
-% The coordinates of cities and counties
-coordinate_path_win = [root_path_win 'Data/city_county_lists/'];
-
