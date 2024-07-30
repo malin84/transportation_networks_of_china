cad02aad879b802954ca4204d76611d1ef6b84fa
Lin Ma <malin84@gmail.com>
2024-07-30 14:06:49 +0800

    Update compile_c_files_updated.m

--- sample_codes/compile_c_files_updated.m
+++ sample_codes/compile_c_files_updated.m
@@ -1,4 +1,11 @@
-% This script will compile all the C files of the registration methods
+% This script will compile all the C files of the registration methods in the package Accurate Fast Marching
+% 
+% This is the updated version that added the option "-compatibleArrayDims" 
+% so the files could be compiled correctly in newer versions of Matlab.
+% Please use this file to replace "compile_c_files.m" in the original package, 
+% and call this file to recompile the source codes. 
+% Incorrectly compiled source code will deliver negative values when computing distance.
+
 cd('functions');
 files=dir('*.c');
 clear msfm2d
